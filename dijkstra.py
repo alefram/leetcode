ef4c55bab26756a00b77328afed5dcf039de980d
alex <cuatroalejandro@gmail.com>
2022-02-23 13:22:40 -0400

    add graphs for dijkstra algorithm

---
+++ dijkstra.py
@@ -0,0 +1,26 @@
+# hash table
+graph["start"] = {}
+graph["start"]["a"] = 6
+graph["start"]["b"] = 2
+graph["a"] = {}
+graph["a"]["fin"] = 1
+
+graph["b"] = {}
+graph["b"]["a"] = 3
+graph["b"]["fin"] = 5
+graph["fin"] = {}
+
+#cost table
+infinity = float('inf')
+costs = {}
+costs["a"] = 6
+costs["b"] = 2
+costs["fin"] = infinity
+
+# parents
+parents = {}
+parents["a"] = "start"
+parents["b"] = "start"
+parents["fin"] = None
+
+node_processed = []
